1b0ff338741cb0d2cc8e15a193be0592f33c8773
GaoJun <912154813@qq.com>
2015-06-17 17:26:15 +0800

    完成遍历省市县三级列表的功能。

--- src/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -15,7 +15,7 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 			+"id integer primary key autoincrement,"
 			+"city_name text,"
 			+"city_code text,"
-			+"province_code text)";
+			+"province_id integer)";
 	public static final String CREATE_COUNTY = "create table County("
 			+"id integer primary key autoincrement,"
 			+"county_name text,"
